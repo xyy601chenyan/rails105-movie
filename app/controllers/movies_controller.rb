c80f2c5bee919b2a1122f166f690112867baacdd
xyy601chenyan <xyy545688506@gmail.com>
2017-03-30 21:24:36 +0800

    implement movies#edit and update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,6 +21,16 @@ def show
   @movie=Movie.find(params[:id])
 end
 
+def edit
+  @movie=Movie.find(params[:id])
+end
+
+def update
+  @movie=Movie.find(params[:id])
+  @movie.update(movie_params)
+  redirect_to movies_path, notice: "Update Success"
+end
+
  private
 
  def movie_params
