64cceac4f8986ac7cfd8601f3e907ac2f16a22ef
xyy601chenyan <xyy545688506@gmail.com>
2017-04-03 17:58:04 +0800

    use before_action to find_movie_and_check_permission

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
 before_action :authenticate_user! , only: [:new,:create,:edit,:update,:destroy]
-
+before_action :find_movie_and_check_permission, only:[:edit,:update,:destroy]
 
  def index
    @movies=Movie.all
@@ -28,17 +28,13 @@ def show
 end
 
 def edit
-  @movie=Movie.find(params[:id])
-  if current_user!=@movie.user
-    redirect_to root_path, alert: "You have no permission."
-  end
+
+
 end
 
 def update
-  @movie=Movie.find(params[:id])
-  if current_user!=@movie.user
-    redirect_to root_path, alert: "You have no permission."
-  end
+  
+
   if @movie.update(movie_params)
   redirect_to movies_path, notice: "Update Success"
 else
@@ -47,10 +43,8 @@ end
 end
 
 def destroy
-@movie=Movie.find(params[:id])
-if current_user!=@movie.user
-  redirect_to root_path, alert: "You have no permission."
-end
+
+
 @movie.destroy
 flash[:alert]="Movie deleted"
 redirect_to movies_path
@@ -60,6 +54,13 @@ end
 
  def movie_params
    params.require(:movie).permit(:title, :description)
+ end
+
+ def find_movie_and_check_permission
+   @movie=Movie.find(params[:id])
+   if current_user!=@movie.user
+     redirect_to root_path, alert:"You have no permission."
+   end
  end
 
 
