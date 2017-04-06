88f3f1f8f796c44f645fea75694749ad9781c27a
xyy601chenyan <xyy545688506@gmail.com>
2017-04-06 20:53:06 +0800

    join or quit movie

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,18 @@ class User < ApplicationRecord
   has_many :movie_relationships
   has_many :participated_movies, :through=> :movie_relationships, :source=> :movie
 
+#判断当前user是否为movie群组成员
   def is_member_of?(movie)
     participated_movies.include?(movie)
   end
-  
+
+  def join!(movie)
+    participated_movies << movie
+  end
+
+  def quit!(movie)
+    participated_movies.delete(movie)
+  end
+
+
 end
