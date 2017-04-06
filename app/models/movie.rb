f7874e5b0f930af38628c9ac48fbfdc3f4782f09
xyy601chenyan <xyy545688506@gmail.com>
2017-04-06 21:42:29 +0800

    user in or out movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,17 @@ class Movie < ApplicationRecord
 
   has_many :movie_relationships
   has_many :members, through: :movie_relationships, source: :user
+
+  def has_member?(user)
+    members.include?(user)
+  end
+
+  def in!(user)
+    members << user
+  end
+
+  def out!(user)
+    members.delete(user)
+  end
+
 end
