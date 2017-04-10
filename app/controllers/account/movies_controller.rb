9606fe566cb802a17e148907b44ea32edac4629a
xyy601chenyan <xyy545688506@gmail.com>
2017-04-10 14:51:24 +0800

    display user's created movies in my movies

--- app/controllers/account/movies_controller.rb
+++ app/controllers/account/movies_controller.rb
@@ -1,6 +1,6 @@
 class Account::MoviesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @movies=current_user.participated_movies.recent
+    @movies=current_user.movies.recent
   end
 end
